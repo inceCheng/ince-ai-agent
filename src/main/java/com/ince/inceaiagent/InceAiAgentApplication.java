9812371c1d582ef376b96f0eb5bc59b738f1f055
ince <incechen@hk1180.com>
2025-05-13 10:04:25 +0800

    init

---
+++ src/main/java/com/ince/inceaiagent/InceAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.ince.inceaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InceAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InceAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ince/inceaiagent/InceAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ince.inceaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InceAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
